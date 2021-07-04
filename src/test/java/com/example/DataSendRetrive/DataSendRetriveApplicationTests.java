0bcf2238bc8f18cc2fd70a9d63600d3d72398550
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-07-04 16:09:31 +0530

    Initial commit

---
+++ src/test/java/com/example/DataSendRetrive/DataSendRetriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataSendRetrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSendRetriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
