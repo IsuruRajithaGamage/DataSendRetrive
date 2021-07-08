9b5a81b9d313743d5fe3df961f327356c3c5de96
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-07-08 19:07:37 +0530

    build create employee REST API!!!

---
+++ src/main/java/com/example/DataSendRetrive/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.DataSendRetrive.service;
+
+import com.example.DataSendRetrive.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+
+}
